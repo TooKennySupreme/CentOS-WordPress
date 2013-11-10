53974671500233f4f69cc155001df3f67b3cd318
GigabyteIO <brian@enviedsolutions.com>
2013-11-10 18:20:34 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 yum -y --exclude=kernel* update
 yum -y install git bc
-git clone https://github.com/MegabyteIO/WP-Droplet.git /usr/local/src/megabyteio
-chmod +x /usr/local/src/megabyteio/scripts/setup
-/bin/bash /usr/local/src/megabyteio/scripts/setup
+git clone https://github.com/MegabyteIO/WP-Droplet.git /usr/local/src/gigabyteio
+chmod +x /usr/local/src/gigabyteio/scripts/setup
+/bin/bash /usr/local/src/gigabyteio/scripts/setup
