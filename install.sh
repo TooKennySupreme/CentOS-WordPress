e88855f779c7dc040e1f82927399b889b727b6f2
GigabyteIO <brian@enviedsolutions.com>
2013-11-10 17:08:01 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+yum -y --exclude=kernel* update
+yum -y install git bc
+git clone https://github.com/MegabyteIO/WP-Droplet.git /usr/local/src/megabyteio
+chmod +x /usr/local/src/megabyteio/scripts/setup
+/bin/bash /usr/local/src/megabyteio/scripts/setup
