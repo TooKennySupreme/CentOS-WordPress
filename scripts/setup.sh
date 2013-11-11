5573f22d053783b4da1a2514edaac961239db626
GigabyteIO <brian@enviedsolutions.com>
2013-11-11 18:44:37 -0500

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -162,7 +162,7 @@ echo "* $(tput setaf 6)Initializing the CentminMod install process via centmin-i
 
 # Change permissions of centmin.sh and centmin-install.exp back to original
 echo "* $(tput setaf 6)Removing centmin-install.exp from CentminMod folder$(tput sgr0)"
-rm -f /$CENTMIN_DIR/$CENTMIN_FOLDER_NAME/centmin-install.exp
+#rm -f /$CENTMIN_DIR/$CENTMIN_FOLDER_NAME/centmin-install.exp
 echo "* $(tput setaf 6)Restoring centmin.sh permissions to original state$(tput sgr0)"
 chmod 644 /$CENTMIN_DIR/$CENTMIN_FOLDER_NAME/centmin.sh
 
