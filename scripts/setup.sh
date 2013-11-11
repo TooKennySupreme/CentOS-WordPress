2a3ec4ef2cddc464d7827f382186d80b1f286c6c
GigabyteIO <brian@enviedsolutions.com>
2013-11-11 17:11:03 -0500

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -144,7 +144,7 @@ echo "* $(tput setaf 6)Changing directory to /$CENTMIN_DIR/$CENTMIN_FOLDER_NAME$
 cd /$CENTMIN_DIR/$INSTALL_FOLDER_NAME
 # Install CentminMod with expect script to automate user inputs
 echo "* $(tput setaf 6)Initializing the CentminMod install process via /$CENTMIN_DIR/$INSTALL_FOLDER_NAME/$SCRIPTS_FOLDER/centmin-install.exp$(tput sgr0)"
-bash /$CENTMIN_DIR/$INSTALL_FOLDER_NAME/$SCRIPTS_FOLDER/centmin-install.exp GigabyteIO password mysqlpass direct
+expect /$CENTMIN_DIR/$INSTALL_FOLDER_NAME/$SCRIPTS_FOLDER/centmin-install.exp
 #expect "New password for the MySQL \"root\" user:"
 #send "PasswordHere\r"
 #expect "Repeat password for the MySQL \"root\" user:"
