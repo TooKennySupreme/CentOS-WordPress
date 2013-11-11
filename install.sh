7e97bb404dd88e37a6da72b6b34177a8b86a6181
GigabyteIO <brian@enviedsolutions.com>
2013-11-10 22:50:15 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-yum -y --exclude=kernel* update
-yum -y install git bc
+yum -q -y --exclude=kernel* update
+yum -q -y install git bc expect
 git clone https://github.com/GigabyteIO/WordPress-Droplet.git /usr/local/src/gigabyteio
 chmod +x /usr/local/src/gigabyteio/scripts/setup.sh
 /bin/bash /usr/local/src/gigabyteio/scripts/setup.sh
