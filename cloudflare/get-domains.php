f23bf414a207523048270442e61c8f313df1245b
GigabyteIO <brian@enviedsolutions.com>
2013-11-12 18:06:46 -0500

    Update get-domains.php

--- cloudflare/get-domains.php
+++ cloudflare/get-domains.php
@@ -1,6 +1,12 @@
 <?php
+
+$apikey = $argv[1];
+$email = $argv[2];
+
 require( dirname(__FILE__) . '/cloudflare-api.php' );
 
-$cf = new cloudflare_api("8afbe6dea02407989af4dd4c97bb6e25");
-$response = $cf->user_create("newuser@example.com", "newpassword", "", "someuniqueid");
+$cf = new cloudflare_api("$email", "$apikey");
+$response = $cf->zone_load_multi();
 echo json_encode($response)
+#rec_edit($domain, $type, $id, $name, $content, $ttl = 1, $mode = 1, $piro = 1, $service = 1, $srvname = 1, $protocol = 1, $weight = 1, $port = 1, $target = 1)
+?>
