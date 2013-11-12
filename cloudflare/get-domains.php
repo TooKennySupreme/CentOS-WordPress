e970e2e9bd169c83c9180415f070886a238e8e37
GigabyteIO <brian@enviedsolutions.com>
2013-11-12 05:21:14 -0500

    Update get-domains.php

--- cloudflare/get-domains.php
+++ cloudflare/get-domains.php
@@ -1,1 +1,6 @@
 <?php
+require( dirname(__FILE__) . '/cloudflare-api.php' );
+
+$cf = new cloudflare_api("8afbe6dea02407989af4dd4c97bb6e25");
+$response = $cf->user_create("newuser@example.com", "newpassword", "", "someuniqueid");
+echo $response
