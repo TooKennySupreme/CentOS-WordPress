c91e9cd26ca81b645b5fa6cbe167580fea62318c
GigabyteIO <brian@enviedsolutions.com>
2013-11-12 18:10:42 -0500

    Update cloudflare.sh

--- scripts/cloudflare.sh
+++ scripts/cloudflare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-result=$( php -f /usr/local/src/gigabyteio/cloudflare/get-domains.php )
+# Calls PHP script to get CloudFlare domains where $1 is the email and $2 is the API key
+result=$( php -f /usr/local/src/gigabyteio/cloudflare/get-domains.php $1 $2 )
 if [ "$result" = "" ] ; then
     echo "Nothing happened."
 else 
