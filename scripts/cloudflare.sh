1ea53aa0f85b44038b61f6a03af9d2f6ba218c05
GigabyteIO <brian@enviedsolutions.com>
2013-11-12 17:48:49 -0500

    Update cloudflare.sh

--- scripts/cloudflare.sh
+++ scripts/cloudflare.sh
@@ -1,1 +1,8 @@
-hi
+#!/bin/bash
+
+result=$( php -f /usr/local/src/gigabyteio/cloudflare/get-domains.php )
+if [ "$result" = "" ] ; then
+    echo "Nothing happened."
+else 
+    echo "$result received."
+fi
