ff1809676e9eff5af18fc72f9df5eda03e0e9460
GigabyteIO <brian@enviedsolutions.com>
2013-11-12 18:11:14 -0500

    Update get-domains.php

--- cloudflare/get-domains.php
+++ cloudflare/get-domains.php
@@ -8,5 +8,5 @@ require( dirname(__FILE__) . '/cloudflare-api.php' );
 $cf = new cloudflare_api("$email", "$apikey");
 $response = $cf->zone_load_multi();
 echo json_encode($response)
-#rec_edit($domain, $type, $id, $name, $content, $ttl = 1, $mode = 1, $piro = 1, $service = 1, $srvname = 1, $protocol = 1, $weight = 1, $port = 1, $target = 1)
+
 ?>
