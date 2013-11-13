b583308461e1b164a6f0064da4c55eafbfbd3644
GigabyteIO <brian@enviedsolutions.com>
2013-11-13 03:34:08 -0500

    Update update-all.php

--- cloudflare/update-all.php
+++ cloudflare/update-all.php
@@ -1,1 +1,14 @@
-http://www.cloudflare.com/docs/client-api.html#s5.1
+<?php
+
+$email = $argv[1];
+$apikey = $argv[2];
+$domain = $argv[3];
+$type = $argv[4];
+$name = $argv[5];
+$content = $argv[6];
+
+require( dirname(__FILE__) . '/cloudflare-api.php' );
+
+$cf = new cloudflare_api("$email", "$apikey");
+$response = $cf->rec_new("$domain", "$type", "$name", "$content");
+var_dump($response);
