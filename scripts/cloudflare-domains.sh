bc209086dc105b4d958592f54e27f10a0f818ecf
GigabyteIO <brian@enviedsolutions.com>
2013-11-13 04:34:02 -0500

    Update cloudflare-domains.sh

--- scripts/cloudflare-domains.sh
+++ scripts/cloudflare-domains.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-# Calls PHP script to get CloudFlare domains where $1 is the email and $2 is the API key
+# Deletes all records and points records to IP address
+# ./cloudflare-domains.sh e-mail api-key type name content
 result=($( php -f /usr/local/src/gigabyteio/cloudflare/get-domains.php $1 $2 ))
 for i in "${result[@]}"
 do
-        echo $i
+        records=($( php -f /usr/local/src/gigabyteio/cloudflare/get-records.php $1 $2 $i ))
+        for j in "${records[@]}"
+        do
+                delete_status=($( php -f /usr/local/src/gigabyteio/cloudflare/delete.php $1 $2 $i $j ))
+                for k in "${delete_status[@]}"
+                do
+                        echo $k
+                done
+        done
+        create_status=($( php -f /usr/local/src/gigabyteio/cloudflare/new-record.php $1 $2 $i $3 $4 $5 ))
 done
