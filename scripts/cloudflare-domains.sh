f9da8aa67a86a60802870bbe7484e04b1d7dcf02
GigabyteIO <brian@enviedsolutions.com>
2013-11-13 03:03:49 -0500

    Update cloudflare-domains.sh

--- scripts/cloudflare-domains.sh
+++ scripts/cloudflare-domains.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # Calls PHP script to get CloudFlare domains where $1 is the email and $2 is the API key
-result=$( php -f /usr/local/src/gigabyteio/cloudflare/get-domains.php $1 $2 )
-if [ "$result" = "" ] ; then
-    echo "Nothing happened."
-else 
-    echo "$result received."
-fi
+result=($( php -f /usr/local/src/gigabyteio/cloudflare/get-domains.php $1 $2 ))
+for i in "${result[@]}"
+do
+        echo $i
+done
