da5751c9b24a20cbe847eb04f44f96f19aa09913
GigabyteIO <brian@enviedsolutions.com>
2013-11-13 04:06:02 -0500

    Update get-domains.php

--- cloudflare/get-domains.php
+++ cloudflare/get-domains.php
@@ -1,5 +1,5 @@
 <?php
-# Returns a list of zone names and IDs ( zone_name_1 zone_id_1 zone_name_2 .... zone_id_5435353453 )
+
 $email = $argv[1];
 $apikey = $argv[2];
 
@@ -10,6 +10,6 @@ $response = $cf->zone_load_multi();
 for ($i = 0; $i < $response->response->zones->count; $i++) {
         if($i != 0) {echo " ";}
         echo $response->response->zones->objs[$i]->zone_name;
-        echo " ";
-        echo $response->response->zones->objs[$i]->zone_id;
+#        echo " ";
+#        echo $response->response->zones->objs[$i]->zone_id;
 }
