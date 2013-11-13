dcee75c4b04483fd2d6026c7a6df57a892e9c1d7
GigabyteIO <brian@enviedsolutions.com>
2013-11-12 23:19:12 -0500

    Update cloudflare-test.php

--- cloudflare/cloudflare-test.php
+++ cloudflare/cloudflare-test.php
@@ -1,1 +1,10 @@
-hi
+<?php
+
+$email = $argv[1];
+$apikey = $argv[2];
+
+require( dirname(__FILE__) . '/cloudflare-api.php' );
+
+$cf = new cloudflare_api("$email", "$apikey");
+$response = $cf->zone_load_multi();
+echo json_encode($response)
