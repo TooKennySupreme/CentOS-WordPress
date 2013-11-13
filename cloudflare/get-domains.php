b81908cf27a53dd08ab5356eace6a8e4ab62a380
GigabyteIO <brian@enviedsolutions.com>
2013-11-13 03:04:29 -0500

    Update get-domains.php

--- cloudflare/get-domains.php
+++ cloudflare/get-domains.php
@@ -7,4 +7,7 @@ require( dirname(__FILE__) . '/cloudflare-api.php' );
 
 $cf = new cloudflare_api("$email", "$apikey");
 $response = $cf->zone_load_multi();
-echo json_encode($response)
+for ($i = 0; $i < $response->response->zones->count; $i++) {
+        if($i != 0) {echo " ";}
+        echo $response->response->zones->objs[$i]->zone_name;
+}
