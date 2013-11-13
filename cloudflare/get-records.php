36dd6c38ce7e4cfd65ddefe5ac94f03bcf26d162
GigabyteIO <brian@enviedsolutions.com>
2013-11-13 04:18:43 -0500

    Update get-records.php

--- cloudflare/get-records.php
+++ cloudflare/get-records.php
@@ -8,11 +8,7 @@ require( dirname(__FILE__) . '/cloudflare-api.php' );
 
 $cf = new cloudflare_api("$email", "$apikey");
 $response = $cf->rec_load_all($domain);
-#for ($i = 0; $i < $response->response->zones->count; $i++) {
-#        if($i != 0) {echo " ";}
-#        echo $response->response->zones->objs[$i]->zone_name;
-#        echo " ";
-#        echo $response->response->zones->objs[$i]->zone_id;
+for ($i = 0; $i < $response->response->recs->count; $i++) {
+        if($i != 0) {echo " ";}
+        echo $response->response->recs->objs[$i]->rec_id;
 }
-var_dump($response);
- #rec_load_all($domain)
