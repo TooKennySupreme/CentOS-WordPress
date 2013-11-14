09b7755cfe6daff21d8dd84b1d423fb0dccce59a
GigabyteIO <brian@enviedsolutions.com>
2013-11-14 01:26:26 -0500

    Update new-srv-record.php

--- cloudflare/new-srv-record.php
+++ cloudflare/new-srv-record.php
@@ -1,5 +1,5 @@
 <?php
-# email apikey domain priority service servicename protocol weight port
+# email apikey domain priority service servicename protocol weight port target
 $email = $argv[1];
 $apikey = $argv[2];
 $domain = $argv[3];
@@ -8,12 +8,13 @@ $name = $argv[3];
 $content = $argv[3];
 $ttl = "3600"; # Recommended by google
 $mode = "0"; # Disabled cloudflare
-$service = $argv[4];
-$srvname = $argv[5];
-$protocol = $argv[6];
-$weight = $argv[7];
-$port = $argv[8];
-$target = $argv[9];
+$priority = $argv[4];
+$service = $argv[5];
+$srvname = $argv[6];
+$protocol = $argv[7];
+$weight = $argv[8];
+$port = $argv[9];
+$target = $argv[10];
 
 
 require( dirname(__FILE__) . '/cloudflare-api.php' );
