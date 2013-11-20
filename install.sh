8a09e9e8922557bfffb2545c724a1e8e46def264
Brian Zalewski <brian@enviedsolutions.com>
2013-11-20 03:24:01 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,21 +13,21 @@ echo "$(tput bold)$(tput setaf 2)Running preliminary tasks$(tput sgr0)"
 echo ""
 echo "* $(tput setaf 6)Changing directory to current users home directory$(tput sgr0)"
 cd ~
-if [ -f /usr/local/src/gigabyteio/install.sh ];
+if [ -f /usr/local/src/megabyteio/install.sh ];
 then
   echo ""
   read -p "$(tput bold)$(tput setaf 3)Warning:$(tput sgr0) The MegabyteIO directory already exists! Delete it and continue the installation? [Y/N] " INSTALL_CHOICE
   case "$INSTALL_CHOICE" in
-    y|Y ) echo "" && echo "* $(tput setaf 6)Removing /usr/local/src/gigabyteio$(tput sgr0)" && rm -rf /usr/local/src/gigabyteio;;
+    y|Y ) echo "" && echo "* $(tput setaf 6)Removing /usr/local/src/megabyteio$(tput sgr0)" && rm -rf /usr/local/src/gigabyteio;;
     n|N ) echo "$(tput setaf 1)$(tput bold)ERROR:$(tput sgr0) Installation aborted by user." && exit;;
     * ) echo "$(tput setaf 1)$(tput bold)ERROR:$(tput sgr0) Invalid input." && exit;;
   esac
 fi
 echo "* $(tput setaf 6)Installing git$(tput sgr0)"
 yum -y --quiet install git
-echo "* $(tput setaf 6)Cloning the GigabyteIO git repository to /usr/local/src/gigabyteio$(tput sgr0)"
-git clone -q https://github.com/GigabyteIO/WordPress-Droplet.git /usr/local/src/gigabyteio
-chmod +x /usr/local/src/gigabyteio/scripts/setup.sh
-echo "* $(tput setaf 6)Initializing setup script from /usr/local/src/gigabyteio/scripts/setup.sh$(tput sgr0)"
+echo "* $(tput setaf 6)Cloning the MegabyteIO git repository to /usr/local/src/megabyteio$(tput sgr0)"
+git clone -q https://github.com/MByteIO/WordPress-Droplet.git /usr/local/src/megabyteio
+chmod +x /usr/local/src/megabyteio/scripts/setup.sh
+echo "* $(tput setaf 6)Initializing setup script from /usr/local/src/megabyteio/scripts/setup.sh$(tput sgr0)"
 echo ""
-/bin/bash /usr/local/src/gigabyteio/scripts/setup.sh
+/bin/bash /usr/local/src/megabyteio/scripts/setup.sh
