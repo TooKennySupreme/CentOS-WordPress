67e5c19b1a2ac017b62f2501bfc2de6fc1abbcaa
Brian Zalewski <brian@enviedsolutions.com>
2013-11-21 23:47:29 -0500

    Update roots.sh

--- multisite/roots.sh
+++ multisite/roots.sh
@@ -1,2 +1,28 @@
-yum install npm grunt
+yum install npm
+npm install -g grunt-cli
 npm install from theme directory
+
+
+# ERRORS:
+/usr/bin/grunt -> /usr/lib/node_modules/grunt-cli/bin/grunt
+npm WARN unmet dependency /usr/lib/node_modules/block-stream requires inherits@'~2.0.0' but will load
+npm WARN unmet dependency undefined,
+npm WARN unmet dependency which is version undefined
+npm WARN unmet dependency /usr/lib/node_modules/fstream requires inherits@'~2.0.0' but will load
+npm WARN unmet dependency undefined,
+npm WARN unmet dependency which is version undefined
+npm WARN unmet dependency /usr/lib/node_modules/fstream-ignore requires inherits@'2' but will load
+npm WARN unmet dependency undefined,
+npm WARN unmet dependency which is version undefined
+npm WARN unmet dependency /usr/lib/node_modules/fstream-npm requires inherits@'2' but will load
+npm WARN unmet dependency undefined,
+npm WARN unmet dependency which is version undefined
+npm WARN unmet dependency /usr/lib/node_modules/glob requires inherits@'2' but will load
+npm WARN unmet dependency undefined,
+npm WARN unmet dependency which is version undefined
+npm WARN unmet dependency /usr/lib/node_modules/npmconf requires inherits@'~2.0.0' but will load
+npm WARN unmet dependency undefined,
+npm WARN unmet dependency which is version undefined
+npm WARN unmet dependency /usr/lib/node_modules/tar requires inherits@'2' but will load
+npm WARN unmet dependency undefined,
+npm WARN unmet dependency which is version undefined
