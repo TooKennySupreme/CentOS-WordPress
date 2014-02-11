f132974b2304c5362a13797295082132f3c4c207
Brian Zalewski <brian@enviedsolutions.com>
2014-02-11 11:07:22 -0800

    Update user-variables.sh

--- user-variables.sh
+++ user-variables.sh
@@ -3,11 +3,11 @@
 # Defaults for all websites
 default_cache='fastcgi'
 
-# Website list
-declare -a site_list=( 'thebestsites.com' 'ultrasound.io' 'enviedsolutions.com' 'bestmetroapps.com' 'megabyte.io' 'laurazalewski.com' )
+# Website lists
+declare -a wordpress_multisite_list=( 'thebestsites.com' 'ultrasound.io' 'enviedsolutions.com' 'bestmetroapps.com' 'megabyte.io' 'laurazalewski.com' )
 
 # Defaults for WordPress installations
-default_theme='https://github.com/MByteIO/TheDefaultThemeGit.git'
-default_mu='https://github.com/MByteIO/MustUseWordPressGit.git'
+default_theme='https://github.com/MByteIO/SimpleAwesomeWordPress.git'
+default_mu='https://github.com/MByteIO/MustUseWordPress.git'
 declare -a default_inactive_plugins=( 'wordpress-seo' 'pods' 'my-shortcodes' 'front-end-editor' )
 declare -a active_plugins=( 'google-sitemap-generator' 'redirection' )
