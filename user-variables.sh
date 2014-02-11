2842e7e2f0e7e0bde10220abce5fad00b041c7c1
Brian Zalewski <brian@enviedsolutions.com>
2014-02-11 10:55:48 -0800

    Update user-variables.sh

--- user-variables.sh
+++ user-variables.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-default_type='static'
+# Defaults for all websites
 default_cache='fastcgi'
-declare -a site_list=( 'thebestsites.com' 'ultrasound.io' 'enviedsolutions.com' 'bestmetroapps.com' )
+
+# Website list
+declare -a site_list=( 'thebestsites.com' 'ultrasound.io' 'enviedsolutions.com' 'bestmetroapps.com' 'megabyte.io' 'laurazalewski.com' )
 
 # Defaults for WordPress installations
-default_theme='https://github.com/MByteIO/AnotherTheme.git'
-default_mu='https://github.com/MByteIO/Must-Use-WordPress.git'
+default_theme='https://github.com/MByteIO/TheDefaultThemeGit.git'
+default_mu='https://github.com/MByteIO/MustUseWordPressGit.git'
 declare -a default_inactive_plugins=( 'wordpress-seo' 'pods' 'my-shortcodes' 'front-end-editor' )
 declare -a active_plugins=( 'google-sitemap-generator' 'redirection' )
-
-# Specify cases where defaults should not be used
-theme_overide( 'thebestsites.com', 'https://github.com/MByteIO/AnotherTheme.git' )
-cache_overide( 'thebestsites.com', 'wp_single' )
-
-# Sync newest git release to specific folder
-git_sync( 'git address' '' )
