2988a8b6a95941de8e0f59ebeb58a0f8e5217c01
Brian Zalewski <brian@enviedsolutions.com>
2014-02-11 12:16:12 -0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,11 +1,1 @@
-#
 
-# Install requirements
-yum -y --quiet install git expect
-
-# Clone the git file
-git clone -q https://github.com/MByteIO/CentOS-WordPress.git /usr/local/src/megabyteio
-chmod +x /usr/local/src/megabyteio/scripts/setup.sh
-echo "* $(tput setaf 6)Initializing setup script from /usr/local/src/megabyteio/scripts/setup.sh$(tput sgr0)"
-echo ""
-/bin/bash /usr/local/src/megabyteio/scripts/setup.sh
