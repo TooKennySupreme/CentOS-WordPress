56135a7adea1208048a01c1f9766b3bbff4ad89c
Brian Zalewski <brian@enviedsolutions.com>
2014-02-11 11:33:15 -0800

    Delete memory.sh

--- scripts/memory.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# memory.sh - adjusts memory settings across various configuration files
-
-# Changes shm_size to 256M - What's the optimal shm_size? Any ideas? Commits!
-perl -pi -e 's/apc.shm_size=32M/apc.shm_size=256M/g' /root/centminmod/php.d/apc.ini
