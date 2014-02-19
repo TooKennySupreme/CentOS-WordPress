e9b3893d0774a9bdb45b0007396575863efd76df
Brian Zalewski <brian@enviedsolutions.com>
2014-02-19 15:29:23 -0500

    Update user-variables.sh

--- user-variables.sh
+++ user-variables.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
+# New root password
+root_password='rootpassword'
+
+# Root user name and password
+new_root_username='testroot'
+new_root_password='testpassword'
+
+# API Keys
+cloudflare_api_key=''
+github_api_key=''
+
 # Defaults for all websites
 default_cache='fastcgi'
 
 # Website lists
-declare -a wordpress_multisite_list=( 'thebestsites.com' 'ultrasound.io' 'enviedsolutions.com' 'bestmetroapps.com' 'megabyte.io' 'laurazalewski.com' )
+declare -a wordpress_multisite_list=( 'thebestsites.com' 'enviedsolutions.com' 'bestmetroapps.com' 'megabyte.io' )
 declare -a wordpress_single_list=( 'laurazalewski.com' )
 declare -a static_website_list=( 'ultrasound.io')
 
 # Defaults for WordPress installations
-default_theme='https://github.com/MByteIO/SimpleAwesomeWordPress.git'
-default_mu='https://github.com/MByteIO/MustUseWordPress.git'
+default_theme='https://github.com/MByteIO/SimpleAwesomeWordPress.git'       # Default theme git
+default_mu='https://github.com/MByteIO/MustUseWordPress.git'                # Default must-use plugins git
 declare -a default_inactive_plugins=( 'wordpress-seo' 'pods' 'my-shortcodes' 'front-end-editor' )
 declare -a active_plugins=( 'google-sitemap-generator' 'redirection' )
