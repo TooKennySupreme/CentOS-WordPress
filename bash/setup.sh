8f5049d3e8e9276e0ebca9db25e29d4fa2e8641b
Brian Zalewski <brian@enviedsolutions.com>
2014-02-25 17:22:41 -0500

    Update setup.sh

--- bash/setup.sh
+++ bash/setup.sh
@@ -46,4 +46,5 @@ perl -pi -e 's/nginx centminmod/MegabyteIO/g' "$centmin_setup"
 # Run the Centmin Mod installation
 chmod +x "$expect_dir"'centmin-install.exp'
 chmod +x "$centmin_setup"
+cd "$centmin_dir"
 "$expect_dir"'centmin-install.exp' "$root_password" 'memcached' "$memcached_password" "$centmin_setup"
