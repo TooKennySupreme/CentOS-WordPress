10822c09ddd2caf7fb6b0919b2a479d781cf0cac
Brian Zalewski <brian@enviedsolutions.com>
2014-02-25 13:03:02 -0500

    Update envoirnment.sh

--- bash/envoirnment.sh
+++ bash/envoirnment.sh
@@ -7,6 +7,7 @@ nginx_conf="$nginx_conf_dir/nginx.conf"                         # nginx.conf loc
 site_conf_dir="$nginx_conf_dir/conf.d"                          # Site-specific conf location
 website_dir='/home/nginx/domains'                               # Directory website files are kept in
 current_dir="$( cd "$( dirname "${[0]}" )" && pwd )"            # Current directory (works unless script is symlinked) - see http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+ssh_conf='/etc/ssh/sshd_config'                                 # SSH configuration file
 
 # Connection specific variables
 mysql_host='localhost'                                          # Host of the MySQL database
@@ -15,16 +16,13 @@ declare -a requires_database=( 'wp-single' 'wp_multi' )         # Types of insta
 # Site specific variables
 site_conf="$site_conf_dir/$site_name.conf"                      # Site-specific configuration named [site_url].conf in the $site_conf_dir
 declare -a site_folders=( 'public' 'private' 'logs' 'backup' )  # Folders to create for each website install
-for i in $site_folders; do                                      # Create folder variable hooks for each member of site_folders
-  site_${i}="$website_dir/$site_name/$i"
-done
 
 # WordPress specific variables
 wp_config=""                                                    # WordPress configuration file location
 wp_core_folder=""                                               # Location of the WordPress core files
 
 # CentminMod variables
-centmin_dl_url='http://centminmod.com/download/'                # Remote directory containing Centmin file
+centmin_dl_url='http://centminmod.com/download'                 # Remote directory containing Centmin file
 centmin_filename='centmin-v1.2.3-eva2000.06.zip'                # Centmin zip file
 centmin_dir="$source_dir/centmin-v1.2.3mod"                     # Centmin unzipped directory
 
