7d4ea4b63131ce25afcd87bd84554871344cb0ca
Brian Zalewski <brian@enviedsolutions.com>
2014-02-25 16:52:44 -0500

    Update envoirnment.sh

--- bash/envoirnment.sh
+++ bash/envoirnment.sh
@@ -30,6 +30,8 @@ megabyteio_dir="$source_dir"'megabyteio/'                         # MegabyteIO d
 declare -a megabyteio_folders=( 'bash' 'expect' 'php' 'modules' 'confs' 'misc' )
 for i in "${megabyteio_folders[@]}"			         # Create folder directory variables from megabyteio_folders	  
 do
-	declare $i_dir=""$megabyteio_dir""$i"/"
+	echo $i
+	echo $1
+	declare $i_dir=$megabyteio_dir
 	echo $i_dir
 done
