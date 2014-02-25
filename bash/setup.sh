14a99f0979c66644fee5c2ae1bed7c155bd8bba0
Brian Zalewski <brian@enviedsolutions.com>
2014-02-25 18:59:57 -0500

    Update setup.sh

--- bash/setup.sh
+++ bash/setup.sh
@@ -54,6 +54,7 @@ for i in "$confs_dir/nginx/*"
 do
   echo $i
 done
+
 # Disable APC CLI in both the apc.ini file and the php.ini file
 perl -pi -e 's/apc.enable_cli=1/apc.enable_cli=0/g' /root/centminmod/php.d/apc.ini
 echo "apc.enable_cli = Off" >> /usr/local/lib/php.ini
