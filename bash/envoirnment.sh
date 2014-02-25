28816ba28cd268dc92cd0222a3a2b9db9d80b11a
Brian Zalewski <brian@enviedsolutions.com>
2014-02-25 16:31:43 -0500

    Update envoirnment.sh

--- bash/envoirnment.sh
+++ bash/envoirnment.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
 # Envoirnment specific variables
-nginx_conf_dir='/usr/local/nginx/'                              # Directory of nginx install file
-source_dir='/usr/local/src/'                                    # Directory of the MegabyteIO folder
-nginx_conf="$nginx_conf_dir"'nginx.conf'                        # nginx.conf location
-site_conf_dir="$nginx_conf_dir"'conf.d/'                        # Site-specific conf location
-website_dir='/home/nginx/domains/'                              # Directory website files are kept in
-ssh_conf='/etc/ssh/sshd_config'                                 # SSH configuration file
+nginx_conf_dir='/usr/local/nginx/'                               # Directory of nginx install file
+source_dir='/usr/local/src/'                                     # Directory of the MegabyteIO folder
+nginx_conf="$nginx_conf_dir"'nginx.conf'                         # nginx.conf location
+site_conf_dir="$nginx_conf_dir"'conf.d/'                         # Site-specific conf location
+website_dir='/home/nginx/domains/'                               # Directory website files are kept in
+ssh_conf='/etc/ssh/sshd_config'                                  # SSH configuration file
 
 # Connection specific variables
-mysql_host='localhost'                                          # Host of the MySQL database
-declare -a requires_database=( 'wp-single' 'wp_multi' )         # Types of installations that need databases
+mysql_host='localhost'                                           # Host of the MySQL database
+declare -a requires_database=( 'wp-single' 'wp_multi' )          # Types of installations that need databases
 
 # Site specific variables
-site_conf="$site_conf_dir"'$site_name.conf'                     # Site-specific configuration named [site_url].conf in the $site_conf_dir
-declare -a site_folders=( 'public' 'private' 'logs' 'backup' )  # Folders to create for each website install
+site_conf="$site_conf_dir"'$site_name.conf'                      # Site-specific configuration named [site_url].conf in the $site_conf_dir
+declare -a site_folders=( 'public' 'private' 'logs' 'backup' )   # Folders to create for each website install
 
 # WordPress specific variables
-wp_config=""                                                    # WordPress configuration file location
-wp_core_folder=""                                               # Location of the WordPress core files
+wp_config=""                                                     # WordPress configuration file location
+wp_core_folder=""                                                # Location of the WordPress core files
 
 # CentminMod variables
 centmin_dl_url='http://centminmod.com/download/'                 # Remote directory containing Centmin file
 centmin_filename='centmin-v1.2.3-eva2000.06.zip'                 # Centmin zip file
-centmin_dir="$source_dir"'centmin-v1.2.3mod/'                     # Centmin unzipped directory
+centmin_dir="$source_dir"'centmin-v1.2.3mod/'			 # Centmin unzipped directory
 
 # MegabyteIO specific directory structure
 megabyteio_dir="$source_dir"'megabyteio'                         # MegabyteIO directory
 declare -a megabyteio_folders=( 'bash' 'expect' 'php' 'modules' 'confs' 'misc' )
-for i in "${megabyteio_folders[@]}"
+for i in "${megabyteio_folders[@]}"			         # Create folder directory variables from megabyteio_folders	  
 do
-  echo $i_dir
+	echo $i_dir
 	$i_dir="$megabyteio_dir""$i"'/'
 done
