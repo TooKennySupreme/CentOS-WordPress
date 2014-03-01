679ac9269deca3596426011d4c141dbc69130219
Brian Zalewski <MByteIO@users.noreply.github.com>
2014-03-01 03:27:27 -0500

    Update user-variables.sh

--- bash/user-variables.sh
+++ bash/user-variables.sh
@@ -10,23 +10,14 @@ new_root_password='testpassword'
 # New SSH port
 new_ssh_port='8388'
 
-# API Keys
-cloudflare_email=''
-cloudflare_api_key=''
-github_api_key=''
-
-# Defaults for all websites
-default_cache='fastcgi'
-
 # Website lists
-declare -a wordpress_multisite_list=( 'megabyte.io' 'bestwebapps.net' 'enviedsolutions.com' 'bestmetroapps.com' 'thebestsites.com' 'laurazalewski.com' 'ultrasound.io' ) # First website in list is the primary domain
-declare -a wordpress_single_list=( 'laurazalewski.com' )
-declare -a static_website_list=( 'pharmasonic.net' )
+declare -a wordpress_multisite_list=( 'megabyte.io' 'bestmetroapps.com' 'thebestsites.com' 'laurazalewski.com' ) # First website in list is the primary domain
+declare -a static_website_list=( 'ultrasound.io' 'enviedsolutions.com' )
 
 # Defaults for WordPress installations
 wordpress_username='testwordpress'
 wordpress_password='testwordpress'
 default_theme='https://github.com/MByteIO/SimpleAwesomeWordPress.git'       # Default theme git
 default_mu='https://github.com/MByteIO/MustUseWordPress.git'                # Default must-use plugins git
-declare -a default_inactive_plugins=( 'wordpress-seo' 'pods' 'my-shortcodes' 'front-end-editor' )
+declare -a default_inactive_plugins=( 'wordpress-seo' 'pods' 'my-shortcodes' )
 declare -a active_plugins=( 'google-sitemap-generator' 'redirection' )
