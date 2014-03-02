d5102586615b8c173713e527c4c6513ac6270fcd
Brian Zalewski <MByteIO@users.noreply.github.com>
2014-03-02 05:04:37 -0500

    Update wp-mu-config.php

--- php/wp-mu-config.php
+++ php/wp-mu-config.php
@@ -47,7 +47,7 @@ define( 'WPLANG', '' ); // U.S. English (en_US) by default
 // define( 'WP_LANG_DIR', dirname(__FILE__) . '/languages');
 
 /* Theme settings */
-define('WP_DEFAULT_THEME', 'roots' );
+define('WP_DEFAULT_THEME', 'megabyteio' );
 // define('TEMPLATEPATH', '/absolute/path/to/wp-content/themes/active-theme');
 // define('STYLESHEETPATH', '/absolute/path/to/wp-content/themes/active-theme');
 
