42a5d2d6e7249321bbd4d2a20c6c08b3d11a4a06
Brian Zalewski <MByteIO@users.noreply.github.com>
2014-03-02 02:33:12 -0500

    Update user-variables.sh

--- bash/user-variables.sh
+++ bash/user-variables.sh
@@ -23,4 +23,4 @@ custom_backend='cms'                                                        # Cu
 default_theme='https://github.com/MByteIO/MegabyteIO-Theme.git'             # Default theme git
 default_mu='https://github.com/MByteIO/MegabyteIO-MustUse.git'                # Default must-use plugins git
 declare -a active_plugins=( 'google-sitemap-generator' 'redirection' )
-declare -a inactive_plugins=( 'wordpress-seo' 'pods' 'my-shortcodes' )
+declare -a inactive_plugins=( 'wordpress-seo' 'pods' )
