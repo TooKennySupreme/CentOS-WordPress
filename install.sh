5b2032b8fa63890d91fe056db10078782a55c405
Brian Zalewski <MByteIO@users.noreply.github.com>
2014-03-02 20:26:40 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,17 @@ cd ~
 yum -y install git expect
 git clone https://github.com/MByteIO/CentOS-WordPress.git /usr/local/src/megabyteio
 chmod +x /usr/local/src/megabyteio/bash/setup.sh
-/bin/bash /usr/local/src/megabyteio/bash/setup.sh
+echo ""
+echo ""
+echo "$(tput bold)$(tput setaf 6)CentOS WordPress$(tput sgr0)"
+echo "Home URL  : http://megabyte.io"
+echo "Github URL: https://github.com/MByteIO/CentOS-WordPress"
+echo "Author    : Brian Zalewski"
+echo ""
+echo "Credit to CentminMod! Check out their website at http://centminmod.com!"
+echo ""
+echo ""
+echo "Instructions:"
+echo ""
+echo "$(tput bold)$(tput setaf 2)1. Edit the file located at /usr/local/src/megabyteio/bash/user-variables.sh with your own information.$(tput sgr0)"
+echo "$(tput bold)$(tput setaf 2)2. Run the fully automated installation by entering the following command: /bin/bash /usr/local/src/megabyteio/bash/setup.sh$(tput sgr0)"
