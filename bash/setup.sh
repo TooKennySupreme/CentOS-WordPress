96ce0a08b1e215524cd321e9af09a95bbd7f63a8
Brian Zalewski <MByteIO@users.noreply.github.com>
2014-03-02 23:43:07 -0500

    Update setup.sh

--- bash/setup.sh
+++ bash/setup.sh
@@ -88,7 +88,7 @@ perl -pi -e 's/apc.enable_cli=1/apc.enable_cli=0/g' /root/centminmod/php.d/apc.i
 echo "apc.enable_cli = Off" >> /usr/local/lib/php.ini
 
 # Changes shm_size to 256M - What's the optimal shm_size? Any ideas?
-perl -pi -e 's/apc.shm_size=32M/apc.shm_size=256M/g' /root/centminmod/php.d/apc.ini
+perl -pi -e 's/apc.shm_size=32M/apc.shm_size=512M/g' /root/centminmod/php.d/apc.ini
 
 # Save passwords to root folder in file named .passwords
 echo 'mysql_root_password='"$mysql_password" >> '/root/.passwords'
