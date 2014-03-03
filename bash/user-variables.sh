a4aa3891e69c443c3456dd75549215a7eebb7e41
Brian Zalewski <MByteIO@users.noreply.github.com>
2014-03-02 19:25:51 -0500

    Update user-variables.sh

--- bash/user-variables.sh
+++ bash/user-variables.sh
@@ -12,9 +12,14 @@ new_ssh_port='8388'
 
 # Website lists
 wordpress_multisite_url='megabyte.io'
+wordpress_multisite_title='Megabyte I/O'
 declare -a static_website_list=( 'ultrasound.io' 'enviedsolutions.com' )
 
 # Defaults for WordPress installations
+wordpress_username='testwordpress'
+wordpress_password='testwordpress'
+wordpress_email='brian@enviedsolutions.com'
 default_theme='https://github.com/MByteIO/MegabyteIO-Theme.git'           # Default theme git
 default_theme_folder_name='megabyteio'
 default_mu='https://github.com/MByteIO/MegabyteIO-MustUse.git'            # Default must-use plugins git
+declare -a inactive_plugins=( 'wordpress-seo' 'pods' )
